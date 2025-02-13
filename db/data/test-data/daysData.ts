f98f1c542ab1efa5c9130f467ab281ddb563bd7c
Oksana Herasymenko <oksanagerasymenko@Mac.mynet>
2025-02-13 21:30:44 +0000

    add day data

---
+++ db/data/test-data/daysData.ts
@@ -0,0 +1,9 @@
+module.exports = [
+  { day: "Monday" },
+  { day: "Tuesday" },
+  { day: "Wednesday" },
+  { day: "Thursday" },
+  { day: "Friday" },
+  { day: "Saturday" },
+  { day: "Sunday" },
+];
